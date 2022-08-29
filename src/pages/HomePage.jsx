326e13a625cc6320d68051caec941febdce9ffb4
Jesper Kling <63609498+jesperkling@users.noreply.github.com>
2022-08-29 18:20:48 +0200

    Changed headline and removed things I didn't use.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,8 @@
 import Container from 'react-bootstrap/Container'
-import { useQuery } from 'react-query'
-import TheMovieDBAPI from '../services/TMDBAPI'
 
 const HomePage = () => {
 	return (
-		<Container className="py-3">
+		<Container className="py-3 text-center">
 			<h1>The Movie Database</h1>
 		</Container>
 	)
