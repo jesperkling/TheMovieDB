55fb2124cddb69ce1d81ceee24ed0d3d0a5ea9ae
Jesper Kling <63609498+jesperkling@users.noreply.github.com>
2022-08-29 18:46:49 +0200

    Can now see posters for movies.

--- src/pages/PopularPage.jsx
+++ src/pages/PopularPage.jsx
@@ -1,6 +1,8 @@
+import { ListGroup } from 'react-bootstrap'
 import Container from 'react-bootstrap/Container'
 import { useQuery } from 'react-query'
 import TheMovieDBAPI from '../services/TMDBAPI'
+import { Link } from 'react-router-dom'
 
 const PopularPage = () => {
 	const { data, isLoading, error, isError } = useQuery(['popular'], TheMovieDBAPI.getPopularMovies)
@@ -14,7 +16,23 @@ const PopularPage = () => {
 			{isError && (<p>Error! {error.message}</p>)}
 
 			{data && (
-				<ul>{data.results.map(movie => (<li key={movie.id}>{movie.title}</li>))}</ul>
+				<ListGroup>
+					{data.results.map(movie => 
+						<ListGroup.Item key={movie.id}>
+							<h3>{movie.title}</h3>
+							<p>{movie.release_date}</p>
+							{movie.poster_path 
+								? <img 
+									className='img-fluid'
+									src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`}
+									alt={"Poster of movie: " + movie.title}
+								/>
+								: <p>No poster available</p>
+							}
+							<Link to={`/movies/${movie.id}`}>Read more</Link>
+						</ListGroup.Item>
+					)}
+				</ListGroup>
 			)}
 
 		</Container>
