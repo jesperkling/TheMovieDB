fcb508387c2b7086a14cdd200c4fda118e124191
jesper kling <jesperkling@outlook.com>
2022-10-20 00:32:26 +0200

    Added pagination component with styling but no functionality yet.

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+
+const Pagination = ({ page, numPages, hasPrevPage, onPrevPage, hasNextPage, onNextPage}) => {
+    return (
+        <div className='d-flex justify-content-between align-items-center mt-3'>
+            <div>
+                <Button 
+                    variant="primary" 
+                    onClick={onPrevPage}
+                    disabled={!hasPrevPage}
+                >
+                    Back
+                </Button>
+            </div>
+
+            <div> Page {page} / {numPages}</div>
+
+            <div>
+                <Button 
+                    variant="primary" 
+                    onClick={onNextPage}
+                    disabled={!hasNextPage}
+                >
+                    Next
+                </Button>
+            </div>
+        </div>
+    )    
+}
+
+export default Pagination
