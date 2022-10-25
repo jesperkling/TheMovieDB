5ec8f0208e16d67fcd72e75e0256d2c22d7e0249
jesper kling <jesperkling@outlook.com>
2022-10-26 01:49:48 +0200

    changed paragraphs to card tags instead

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,9 +7,9 @@ import { Link } from 'react-router-dom'
 const MovieCard = ({ data }) => {
     return (
         <Row xs={1} md={2} lg={3} xl={4} className='g-4'>
-                {data.results.map(movie =>
-                    <Col>
-                        <Card className='my-3 text-center h-100' style={{ width: '18rem' }} key={movie.id}>
+                {data.results.map((movie, id) =>
+                    <Col key={id}>
+                        <Card className='my-3 text-center h-100' style={{ width: '18rem' }} key={id}>
                             <Card.Title className='my-3'>{movie.title}</Card.Title>
                             {movie.poster_path 
                                 ? <Card.Img 
@@ -19,12 +19,13 @@ const MovieCard = ({ data }) => {
                                     />
                                     : <p>No poster available</p>
                             }
-                                <Card.Text className='p-3'>
-                                    <p><strong>Released:</strong> {movie.release_date}</p>
-                                    <p><strong>Rating:</strong> {movie.vote_average} ({movie.vote_count})</p>
-                                    <p>{movie.overview}</p>
+                                <Card.Body className='p-3'>
+                                    <Card.Subtitle>Released:</Card.Subtitle> 
+                                    {movie.release_date}
+                                    <Card.Subtitle>Rating:</Card.Subtitle> {movie.vote_average} ({movie.vote_count})
+                                    <Card.Text>{movie.overview}</Card.Text>
                                     <Link to={`/movies/${movie.id}`}>Read more</Link>
-                            </Card.Text>
+                            </Card.Body>
                         </Card>
                     </Col>
                 )}
