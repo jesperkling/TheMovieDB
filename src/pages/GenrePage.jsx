b4107089ed8d47ab8699150916eb97ae235b4236
jesper kling <jesperkling@outlook.com>
2022-10-27 11:58:10 +0200

    now page scroll to top when changing page

--- src/pages/GenrePage.jsx
+++ src/pages/GenrePage.jsx
@@ -3,12 +3,17 @@ import { useParams, useSearchParams } from 'react-router-dom'
 import Pagination from '../components/Pagination'
 import MovieCard from '../components/MovieCard'
 import useMoviesByGenre from '../hooks/useMoviesByGenre'
+import { useEffect } from 'react'
 
 const GenrePage = () => {
 	const [searchParams, setSearchParams] = useSearchParams({ page: 1 })
 	const page = searchParams.get('page') ? Number(searchParams.get('page')) : null
 	const { id, name } = useParams()
 	const { data, isLoading, isError, error } = useMoviesByGenre(id, page)
+
+	useEffect(() => {
+		window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+	}, [data])
 	
 	return (
 		<Container className='text-center'>
